6d572a1b4067dae5c937d41a68f761fcdd908089
Alexey <petrenko012345@yandex.ru>
2024-12-06 13:21:16 +0500

    clear master

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import './App.css';
-import ComponentLocalStorage from './components/componentLocalStorage';
-import ComponentFetch from './components/componentFetch';
-import ComponentHover from './components/componentHover';
-import ComponentViewportSize from './components/componentViewportSize';
-import ComponentWindowScroll from './components/componentWindowScroll';
+
 
 function App() {
 
   return (
     <>
-      <ComponentFetch />
-      {/* <ComponentLocalStorage /> */}
-      {/* <ComponentHover/> */}
-      {/* <ComponentViewportSize/> */}
-      {/* <ComponentWindowScroll/> */}
+      123
     </>
   );
 }
